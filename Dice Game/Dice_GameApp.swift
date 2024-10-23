d84ff3fbf60a00967edb5d552cc0cf47074c8635
Chase <Chashiguchi@gmail.com>
2024-10-23 09:32:13 -0500

    Initial Commit

---
+++ Dice Game/Dice_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_GameApp.swift
+//  Dice Game
+//
+//  Created by Chase Hashiguchi on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Dice_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dice GameTests/Dice_GameTests.swift
@@ -0,0 +1,16 @@
+//
+//  Dice_GameTests.swift
+//  Dice GameTests
+//
+//  Created by Chase Hashiguchi on 10/23/24.
+//
+
+import Testing
+
+struct Dice_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
